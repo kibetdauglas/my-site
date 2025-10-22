ed282feea1f057e7d2434bcd3a9908e55bd7a2ae
kibetdauglas <kibetdauglas@gmail.com>
2025-10-22 10:38:02 +0000

    Proper initial commit with all website files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello 👋</h1>
+      <p>My first website is working!</p>
+    </div>
+  );
+}
+
+export default App;
+
+
